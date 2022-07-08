428c122dd0b631140390c6630804affe1963475c
dummyx <40601688+dummyx@users.noreply.github.com>
2022-07-08 18:14:41 +0900

    Add path related functions

--- pygeon/messenger/messenger.py
+++ pygeon/messenger/messenger.py
@@ -1,8 +1,16 @@
 from typing import Protocol
 from message import Message
+import os
 
 
 class Messenger(Protocol):
+    @property
+    def file_cache_path(self) -> str:
+        return os.path.join(os.getcwd(), "file_cache")
+
+    def generate_cache_file_path(self, filename: str) -> str:
+        return os.path.join(self.file_cache_path, filename)
+
     @property
     def name(self) -> str:
         return self.__class__.__name__
