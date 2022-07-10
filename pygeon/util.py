d436a80c3d3573beec530b2d0651957897a2f65b
dummyx <40601688+dummyx@users.noreply.github.com>
2022-07-10 12:36:11 +0900

    Some formatting

--- pygeon/util.py
+++ pygeon/util.py
@@ -5,27 +5,26 @@ import os
 
 
 def run_in_thread(func: Callable, args: tuple):
-    thread = Thread(target=func,args=args)
+    thread = Thread(target=func, args=args)
     thread.start()
 
-def download_to_cache(url: str, directory: str,filename: str, headers=None):
+
+def download_to_cache(url: str, directory: str, filename: str, headers=None):
     with requests.get(url, stream=True, headers=headers) as r:
         r.raise_for_status()
         content_type = r.headers["content-type"]
 
         filename = rename_with_proper_suffix(filename, content_type)
 
-        file_path = os.path.join(directory,filename)
-        with open(file_path, 'wb') as f:
+        file_path = os.path.join(directory, filename)
+        with open(file_path, "wb") as f:
             for chunk in r.iter_content(chunk_size=8192):
                 f.write(chunk)
         return file_path
 
+
 def rename_with_proper_suffix(filename: str, content_type: str) -> str:
-    suffix = '.' + content_type.split("/")[1]
+    suffix = "." + content_type.split("/")[1]
     if not filename.endswith(suffix):
         filename += suffix
     return filename
-
-
-
